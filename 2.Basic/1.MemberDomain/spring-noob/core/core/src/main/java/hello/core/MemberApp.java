51244b735aedc3dc0ce31de335f06cdc0fa7917b
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-07-13 01:21:25 +0900

    순수 자바를 이용한 회원 도메인 개발
    순수 자바를 이용한 회원 도메인 개발

---
+++ 2.Basic/1.MemberDomain/spring-noob/core/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) { //psvm 치면 자동으로 main메소드 생성
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP); //id가 Long타입이어서 1뒤에 L붙임
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName()); //soutv
+        System.out.println("findMember = " + findMember.getName());
+
+
+    }
+}
