8485d03a2785b03d3c19ee92177e7c988b153cdd
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-07-25 00:48:10 +0900

    Delete 2.Basic/spring-noob directory

--- 2.Basic/spring-noob/core/core/src/main/java/hello/core/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package hello.core.member;
-
-public interface MemberRepository { //회원 저장소
-    void save(Member member);
-
-    Member findById(Long memberId);
-}
--- 2.Basic/spring-noob/core/core/src/main/java/hello/core/order/OrderService.java
+++
@@ -1,6 +0,0 @@
-package hello.core.order;
-
-public interface OrderService {
-    //주문 도메인 전체 그림의 4.주문결과반환(return)에 해당
-    Order createOrder(Long memberId,String itemName,int itemPrice);
-}
